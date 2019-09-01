94a6d043afbdde26582cae9b5d760e03cd81a60a
Vany <vany8462@gmail.com>
2019-09-01 14:55:10 +0800

    控制坦克向8個方向移動

---
+++ src/main/java/com/javaneversleep/tankwar/Direction.java
@@ -0,0 +1,14 @@
+package com.javaneversleep.tankwar;
+
+public enum Direction {
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT
+}
--- src/main/java/com/javaneversleep/tankwar/GameClient.java
+++ src/main/java/com/javaneversleep/tankwar/GameClient.java
@@ -5,24 +5,28 @@ import com.apple.eawt.Application;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
 public class GameClient extends JComponent {
 
+    private Tank playerTank;
+
     private GameClient() {
+        this.playerTank = new Tank(400,100, Direction.DOWN);
+
         this.setPreferredSize(new Dimension(800,600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+        playerTank.draw(g);
     }
 
     public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame();
+        final JFrame frame = new JFrame();
         frame.setTitle("TANK WAR!");
 
         File imageFile = new File("assets/images/icon.png");
@@ -31,11 +35,35 @@ public class GameClient extends JComponent {
 
         //frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
 
-        GameClient client = new GameClient();
+        final GameClient client = new GameClient();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                boolean u = false, d = false, l = false, r = false;
+                client.playerTank.keyPressed(e);
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyReleased(e);
+
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        while (true) {
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
     }
 }
